205c493415af7b6ebee7a8640d0d4a3d5116e37f
Prafful-Goyal <139034009+Prafful-Goyal@users.noreply.github.com>
2024-01-04 01:45:58 +0530

    Minimize the Maximum of Two Arrays.cpp

--- Day1/Minimize the Maximum of Two Arrays.cpp
+++ Day1/Minimize the Maximum of Two Arrays.cpp
@@ -1,7 +1,23 @@
-    //APPROACH -> BINARY SEARCH
+//PROBLEM STATEMENT
+/*
+We have two arrays arr1 and arr2 which are initially empty. You need to add positive integers to them such that they satisfy all the following conditions:
+
+arr1 contains uniqueCnt1 distinct positive integers, each of which is not divisible by divisor1.
+arr2 contains uniqueCnt2 distinct positive integers, each of which is not divisible by divisor2.
+No integer is present in both arr1 and arr2.
+Given divisor1, divisor2, uniqueCnt1, and uniqueCnt2, return the minimum possible maximum integer that can be present in either array.
+*/
+
+//APPROACH -> BINARY SEARCH
+/*
+    T.C = O(LOG 10^10)
+    S.C = O(1)
+*/
+class Solution {
+public:
     /*
-        T.C = O(LOG 10^10)
-        S.C = O(1)
+    T.C = O(LOG 10^10)
+    S.C = O(1)
     */
     long gcd(long a,long b)
     {
@@ -42,3 +58,4 @@
         bs(1,10000000000,divisor1,divisor2,l,(&ans),uniqueCnt1,uniqueCnt2);
         return ans;
     }
+};
