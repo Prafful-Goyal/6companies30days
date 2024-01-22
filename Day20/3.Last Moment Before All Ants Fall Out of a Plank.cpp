816db0f2ffbd60111458850580726a0fc6562dbd
Prafful-Goyal <139034009+Prafful-Goyal@users.noreply.github.com>
2024-01-22 21:59:53 +0530

    3.Last Moment Before All Ants Fall Out of a Plank.cpp

---
+++ Day20/3.Last Moment Before All Ants Fall Out of a Plank.cpp
@@ -0,0 +1,27 @@
+//PROBLEM STATEMENT
+/*
+We have a wooden plank of the length n units. Some ants are walking on the plank, each ant moves with a speed of 1 unit per second. Some of the ants move to the left, the other move to the right.
+When two ants moving in two different directions meet at some point, they change their directions and continue moving again. Assume changing directions does not take any additional time.
+When an ant reaches one end of the plank at a time t, it falls out of the plank immediately.
+Given an integer n and two integer arrays left and right, the positions of the ants moving to the left and the right, return the moment when the last ant(s) fall out of the plank.
+*/
+//CODE
+/*
+  T.C = O(N)
+  S.C = O(1)
+*/
+class Solution {
+public:
+    int getLastMoment(int n, vector<int>& left, vector<int>& right) {
+        int mn = 0;
+        for(auto it:right)
+        {
+            mn = max(n-it,mn);
+        }
+        for(auto it:left)
+        {
+            mn = max(it,mn);
+        }
+        return mn;
+    }
+};
